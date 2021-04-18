1b21b565db4e1dadbe8b7c3dab85dc5d76420f76
Yoel-18 <63850050+Yoel-18@users.noreply.github.com>
2021-04-18 19:26:41 +0900

    udp

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+var wg sync.WaitGroup
+var sum int
+
+func main() {
+	wg.Add(1)
+	go testGo()
+	wg.Wait()
+	fmt.Println("합: ", sum)
+}
+func testGo() {
+	defer wg.Done()
+	for i := 0; i < 100000; i++ {
+		go func() {
+			sum += 1
+		}()
+	}
+	fmt.Println("바깥쪽")
 }
