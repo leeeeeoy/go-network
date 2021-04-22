6b9c3914769c0d95744a71bd53225e794b497f46
Yoel-18 <63850050+Yoel-18@users.noreply.github.com>
2021-04-22 23:18:08 +0900

    udp server

---
+++ ch06/tftp.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"github.com/leeeeeoy/go-network/ch06/tftp"
+)
+
+var (
+	address = flag.String("a", "127.0.0.1:69", "listen address")
+	payload = flag.String("p", "standing.svg", "file to serve to clients")
+)
+
+func main() {
+	flag.Parse()
+
+	p, err := ioutil.ReadFile(*payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := tftp.Server{
+		Payload: p,
+	}
+	log.Fatal(s.ListenAndServe(*address))
+}
